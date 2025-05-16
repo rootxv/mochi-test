aa514c1102fe119511e2490962f194242c5dd0ad
rootxv <rootxv@gmail.com>
2025-05-16 23:33:01 +0000

    fix test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,72 @@
 import streamlit as st
 import pandas as pd
 import math
+import matplotlib
 from pathlib import Path
+from streamlit_gsheets import GSheetsConnection
+from datetime import date
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Mochi Health Mood Tracker',
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+# conn = st.connection("gsheets", type=GSheetsConnection)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+# mood_df = conn.read()
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Print results.
+# st.dataframe(mood_df)
+# sql = 'select mood, count(mood) as responses from "Sheet 1" where date = CURRENT_DATE() group by 1'
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+#graph_df = conn.query(spreadsheet=url, sql=sql)
+#st.dataframe(graph_df)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+# def submit_values(df, date_value, mood_value, note_value):
+#     df.loc[len(df)] = [date_value, mood_value, note_value]
+#     df = conn.update(worksheet = "Sheet 1", data = df)
+#     st.cache_data.clear()
+#     st.rerun()
+# # -----------------------------------------------------------------------------
+# # Draw the actual page
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+# # Set the title that appears at the top of the page.
+# '''
+# # Mochi Health Mood Tracker
 
-    return gdp_df
+# '''
 
-gdp_df = get_gdp_data()
+# # Add some spacing
+# ''
+# ''
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+# with st.form("mood_form"):
+#     st.write("Select your mood for today and add a note if want to include more details.")
+#     mood_val = st.feedback("faces")
+#     note_val = st.text_area("Additional info:")
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+#     submitted = st.form_submit_button("Submit")
+#     if submitted:
+#         submit_values(mood_df, date.today(), mood_val, note_val)
+#         st.write("Thank you for the feedback!")
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+# ''
+# ''
+# # aggregate the data
+# agg_df = mood_df.groupby('Mood').count()
 
-st.header(f'GDP in {to_year}', divider='gray')
+# st.header('Moods today', divider='gray')
 
-''
+# ''
+# print(agg_df)
+# st.line_chart(agg_df)
 
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+# ''
+# ''
